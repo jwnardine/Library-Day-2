a033affd6e1e1603e3919852a12de01237c7a6a5
Jon Nardine and matt knutson <jwnardine@gmail.com>
2016-03-03 11:58:00 -0800

    authors is working

--- app/app.php
+++ app/app.php
@@ -2,31 +2,23 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Book.php";
     require_once __DIR__."/../src/Author.php";
-
     $app = new Silex\Application();
-
     $app['debug'] = true;
-
     $server = 'mysql:host=localhost;dbname=library';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
-
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
-
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
-
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('authors' => Author::getAll(), 'books' => Book::getAll()));
     });
-
     $app->get("/authors", function() use ($app) {
         return $app['twig']->render('authors.html.twig', array('authors' => Author::getAll()));
     });
-
     $app->post("/authors", function() use ($app) {
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
@@ -34,17 +26,14 @@
         $author_name->save();
         return $app['twig']->render('authors.html.twig', array('authors' => Author::getAll()));
     });
-
     $app->get("/authors/{id}", function($id) use ($app) {
         $author = Author::find($id);
         return $app['twig']->render('author.html.twig', array('author' => $author, 'books' => $author->getBooks(), 'all_books' => Book::getAll()));
     });
-
     $app->get("/authors/{id}/edit", function($id) use($app){
         $author = Author::find($id);
         return $app['twig']->render('author_edit.html.twig', array('author' => $author));
     });
-
     $app->patch("authors/{id}", function($id) use ($app) {
         $first_name = $_POST['first_name'];
         $last_name = $_POST['last_name'];
@@ -53,7 +42,8 @@
         return $app['twig']->render('author.html.twig', array('author' => $author, 'books' => $author->getBooks()));
     });
 
-    $app->delete("/authors/{id}", function($id) use ($app) {
+    /*Delete a single author*/
+    $app->delete("/delete_author/{id}", function($id) use ($app) {
         $author = Author::find($id);
         $author->delete();
         return $app['twig']->render('librarian.html.twig', array('authors' => Author::getAll()));
@@ -65,23 +55,19 @@
         $author->addBook($book);
         return $app['twig']->render('author.html.twig', array('author' => $author, 'authors' => Author::getAll(), 'books' => $author->getBooks(), 'all_books' => Book::getAll()));
     });
-
     $app->get("/books", function() use ($app) {
         return $app['twig']->render('books.html.twig', array('books' => Book::getAll()));
     });
-
     $app->post("/books", function() use ($app) {
         $title = $_POST['title'];
         $book = new Book($title);
         $book->save();
         return $app['twig']->render('books.html.twig', array('books' => Book::getAll()));
     });
-
     $app->get("/books/{id}", function($id) use ($app) {
         $book = Book::find($id);
         return $app['twig']->render('book.html.twig', array('book' => $book, 'authors' => $book->getAuthors(), 'all_authors' => Author::getAll()));
     });
-
     $app->post("/add_authors", function() use($app){
       $author = Author::find($_POST['author_id']);
       $book = Book::find($_POST['book_id']);
@@ -89,7 +75,7 @@
       return $app['twig']->render('book.html.twig', array('book' => $book, 'books' => Book::getAll(), 'authors' => $book->getAuthors(), 'all_authors' => Author::getAll()));
     });
 
-    $app->delete("/delete/{id}", function($id) use ($app) {
+    $app->delete("/delete_book/{id}", function($id) use ($app) {
         $book = Book::find($id);
         $book->delete();
         return $app['twig']->render('books.html.twig', array('books' => Book::getAll()));
@@ -99,15 +85,12 @@
         Book::deleteAll();
         return $app['twig']->render('librarian.html.twig');
     });
-
     $app->post("/delete_authors", function() use ($app) {
         Author::deleteAll();
         return $app['twig']->render('librarian.html.twig');
     });
-
     $app->get("/librarian", function() use ($app) {
         return $app['twig']->render('librarian.html.twig', array('authors' => Author::getAll(), 'books' => Book::getAll()));
     });
-
     return $app;
  ?>
